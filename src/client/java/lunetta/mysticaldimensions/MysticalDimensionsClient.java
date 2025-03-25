713204c1e67bdb7495be160e374a9bb981f7d8f7
Lunetta <alexander.stevens2023@gmail.com>
2025-03-24 20:41:32 -0400

    init commit

---
+++ src/client/java/lunetta/mysticaldimensions/MysticalDimensionsClient.java
@@ -0,0 +1,10 @@
+package lunetta.mysticaldimensions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MysticalDimensionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
